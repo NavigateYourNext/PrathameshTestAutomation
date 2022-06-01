47596c6dccaf6d166c248286d5d89fc6d56b1462
AkshayShete Shete (ZA) <AkshayShete@absa.africa>
2022-06-01 11:12:47 +0200

    Code pushed

--- src/main/java/com/ezsolutions/pages/LoginPage.java
+++ src/main/java/com/ezsolutions/pages/LoginPage.java
@@ -1,13 +1,12 @@
 package com.ezsolutions.pages;
 
-import com.ezsolutions.base.BaseClass;
-import com.ezsolutions.utilities.Utils;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.openqa.selenium.By;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.PageFactory;
+
+import com.ezsolutions.base.BaseClass;
 
 public class LoginPage extends BaseClass {
 
@@ -23,11 +22,13 @@ public class LoginPage extends BaseClass {
 
     private ProductsPage doLogin(){
         return new ProductsPage();
+       
     }
 
     public void enterUserName(String thisUsername){
         driver.findElement(username).sendKeys(thisUsername);
         logger.info("Username entered");
+        Actions a  = new Actions(driver);
     }
 
     public void enterPassword(String thisPassword){
